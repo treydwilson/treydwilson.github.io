db69481334a4a7f7a3715c4398ac6a3fe605f8d2
Trey Wilson <treydwilson86@gmail.com>
2015-10-17 12:01:05 -0500

    Adding multi touch tests

---
+++ test.js
@@ -0,0 +1,17 @@
+$(function() {
+	$(".block").draggable();
+	
+	$(".slider").draggable({ containment: 'parent'});
+	$(".slider").on("drag", function(ev) {
+		var length = ($(ev.target).position().top / 2) + 50;
+		$('.block').height(length);
+		$('.block').width(length);
+		
+		var mc = new Hammer.Manager(document.getElementById('slider-container'));
+		
+		mc.on('rotatemove', function(ev) {
+			$('.color-one').addClass('color-two');
+			$('.color-one').removeClass('color-one');
+		});
+	});
+});
